a9d51f159e230b347ab7dc654dd370d19282ecf5
abhishekydv7 <abhibuddy224@gmail.com>
2026-07-19 23:48:59 +0530

    models and routes

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { registerUser } from "../controllers/authController.js";
+import { registerUser, loginUser, logoutUser, getMe } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
+import { authLimiter } from "../middleware/rateLimiter.js";
 
 const router = express.Router();
 
-router.post('/register', registerUser);
+router.post('/register', authLimiter, registerUser);
+router.post("/login", authLimiter, loginUser);
+router.post("/logout", logoutUser);
+router.get("/me", protect, getMe);
 
 export default router
