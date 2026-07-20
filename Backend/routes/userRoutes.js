6f6492dc54b4932985fc7432844c4518ac185ed9
abhishekydv7 <abhibuddy224@gmail.com>
2026-07-20 15:10:08 +0530

    routes(II)

---
+++ Backend/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  updateUserProfile,
+  updatePassword,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.put("/profile", protect, updateUserProfile);
+router.put("/password", protect, updatePassword);
+
+export default router;
