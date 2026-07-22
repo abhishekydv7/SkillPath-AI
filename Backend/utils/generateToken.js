317d5c2b341028e1ab32266dbd32148d4ae198c6
abhishekydv7 <abhibuddy224@gmail.com>
2026-07-22 20:40:44 +0530

    JWT Authentication

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,18 +1,21 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
-        expiresIn: '30d'
-    });
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 
-    res.cookie('jwt', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production' || !!process.env.
-        CLIENT_URL,
-        sameSite: (process.env.NODE_ENV === 'production' || !!process.env.
-        CLIENT_URL) ? 'none' : 'strict',
-        maxAge: 30 * 24 * 60 * 60 * 1000 
-    })
-}
+  console.log("🍪 Setting JWT cookie");
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    path: "/",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+
+  console.log("🍪 Cookie attached to response");
+};
 
 export default generateToken;
